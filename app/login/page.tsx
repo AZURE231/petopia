becb412c92c4796e2abacd72a6b86fd72a9d9576
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-11-08 14:27:39 +0700

    clean up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,31 +5,29 @@ import LoginForm from "@/components/LoginForm";
 export default function page() {
   return (
     <div className="">
-      {/* Background */}
       <div className="absolute m-auto left-0 right-0 z-50">
         <LoginForm />
       </div>
-      <div className="w-full left-0 top-0 z-0">
-        <div className="flex ">
-          <div className="bg-white h-screen w-1/2">
-            <div className="h-screen relative">
-              <Image
-                className="object-cover"
-                alt="girl and dog"
-                src={"/img/girl_dog.webp"}
-                fill={true}
-              ></Image>
-            </div>
+      {/* Background */}
+      <div className="flex">
+        <div className="bg-white h-screen w-1/2">
+          <div className="h-screen relative">
+            <Image
+              className="object-cover"
+              alt="girl and dog"
+              src={"/img/girl_dog.webp"}
+              fill={true}
+            ></Image>
           </div>
-          <div className="bg-yellow-300 h-screen w-1/2">
-            <div className="h-screen relative">
-              <Image
-                className="object-cover"
-                alt="golden_retriever"
-                src={"/img/pexels-gilberto-reyes.png"}
-                fill={true}
-              ></Image>
-            </div>
+        </div>
+        <div className="bg-yellow-300 h-screen w-1/2">
+          <div className="h-screen relative">
+            <Image
+              className="object-cover"
+              alt="golden_retriever"
+              src={"/img/pexels-gilberto-reyes.png"}
+              fill={true}
+            ></Image>
           </div>
         </div>
       </div>
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -4,7 +4,10 @@ export default function LoginForm() {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <div>
-            <h2 className="">Petopia xin chào</h2>
+            <h2 className="">
+              <span className="text-yellow-300 font-bold">Pet</span>opia xin
+              chào
+            </h2>
             <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl dark:text-white">
               Đăng nhập
             </h1>
@@ -15,7 +18,7 @@ export default function LoginForm() {
                 htmlFor="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your email
+                Emal của bạn
               </label>
               <input
                 type="email"
@@ -31,7 +34,7 @@ export default function LoginForm() {
                 htmlFor="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Password
+                Mật khẩu
               </label>
               <input
                 type="password"
@@ -58,7 +61,7 @@ export default function LoginForm() {
                     htmlFor="remember"
                     className="text-gray-500 dark:text-gray-300"
                   >
-                    Remember me
+                    Nhớ đăng nhập
                   </label>
                 </div>
               </div>
@@ -66,31 +69,33 @@ export default function LoginForm() {
                 href="#"
                 className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
               >
-                Forgot password?
+                Quên mật khẩu?
               </a>
             </div>
             <button
               type="submit"
               className="w-full text-black bg-yellow-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
-              Sign in
+              Đăng nhập
             </button>
-            <button className="w-full text-center px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
-              <img
-                className="w-6 h-6"
-                src="https://www.svgrepo.com/show/475656/google-color.svg"
-                loading="lazy"
-                alt="google logo"
-              />
-              <span>Login with Google</span>
+            <button className="w-full content-end py-2 border flex border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+              <div className="flex gap-2 mx-auto">
+                <img
+                  className="w-6 h-6"
+                  src="https://www.svgrepo.com/show/475656/google-color.svg"
+                  loading="lazy"
+                  alt="google logo"
+                />
+                <span className="">Đăng nhập với Google</span>
+              </div>
             </button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don t have an account yet?{" "}
+              Chưa có tài khoản?{" "}
               <a
                 href="#"
                 className="font-medium text-primary-600 hover:underline dark:text-primary-500"
               >
-                Sign up
+                Đăng ký
               </a>
             </p>
           </form>
