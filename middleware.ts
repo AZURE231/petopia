09899c5868492001d295b8b68521ebfb8865c39e
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2024-04-04 10:52:58 +0700

    fix pet page

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { COOKIES_NAME } from './src/utils/constants';
 
 export default function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/pet')) {
+    const response = NextResponse.rewrite(new URL('/search', request.url));
+    return response;
+  }
+
   if (!request.cookies.has(COOKIES_NAME.ACCESS_TOKEN_SERVER)) {
     const response = NextResponse.rewrite(new URL('/login', request.url));
     response.cookies.set(
@@ -13,5 +18,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/adopt', '/user'],
+  matcher: ['/adopt', '/user', '/pet'],
 };
