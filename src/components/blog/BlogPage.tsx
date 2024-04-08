d0f18a05d44af50f7db769430dcba022f898459f
Minh Hung <acubu6969@gmail.com>
2024-04-08 16:03:22 +0700

    refractor

--- src/components/blog/BlogPage.tsx
+++ src/components/blog/BlogPage.tsx
@@ -1,15 +1,40 @@
-// src/components/BlogPage.tsx
-
 import React from "react";
+import BlogCard from "./BlogCard";
 
-interface BlogPageProps {
-  htmlContent: string; // HTML content for the blog
+interface Blog {
+  id: string;
+  image: string;
+  category: string;
+  title: string;
+  excerpt: string;
 }
 
-const BlogPage: React.FC<BlogPageProps> = ({ htmlContent }) => {
+interface Props {
+  blogTitle: string;
+  htmlContent: string;
+  blogs: Blog[];
+}
+
+const BlogPage: React.FC<Props> = ({ blogTitle, htmlContent, blogs }) => {
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    <div className="container mx-auto mt-10 cols justify-center" style={{ width: "1080px" }}>
+      <h1 className="text-4xl font-bold">{blogTitle}</h1>
+      <div className="w-full text-justify mt-10" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <div className="container mx-auto mt-20 text-lg font-bold">CÓ THỂ BẠN QUAN TÂM</div>
+      <div className="flex justify-center mt-10">
+        <div className="blog-grid grid grid-cols-3 gap-12">
+          {blogs.slice(0, 3).map((blog) => (
+            <BlogCard
+              key={blog.id}
+              id={blog.id}
+              image={blog.image}
+              category={blog.category}
+              title={blog.title}
+              excerpt={blog.excerpt}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/blog/BlogSection.tsx
+++ src/components/blog/BlogSection.tsx
@@ -1,72 +1,29 @@
-'use client';
-import React, { useState } from 'react';
-import Image from 'next/image';
-import BlogCard from './BlogCard';
-import Pagination from '../general/Pagination';
-import { blogs } from '@/app/(pages)/blog/blogs';
-import { useForm } from 'react-hook-form';
-import { IPaginationModel } from '@/src/interfaces/common';
+import React from "react";
+import BlogCard from "./BlogCard";
 
-interface BlogSectionProps {
-  categories: string[];
-  bannerImage: string;
+interface Blog {
+  id: string;
+  image: string;
+  category: string;
+  title: string;
+  excerpt: string;
 }
 
-const BlogSection: React.FC<BlogSectionProps> = ({
-  categories,
-  bannerImage,
-}) => {
+interface Props {
+  blogTitle: string;
+  htmlContent: string;
+  blogs: Blog[];
+}
 
-  const [selectedCategory, setSelectedCategory] = useState<string>(
-    categories[0]
-  );
-
-  const paginationForm = useForm<IPaginationModel>({
-    defaultValues: {
-      pageIndex: 1,
-      pageNumber: 1,
-    }
-  });
-
+const BlogSection: React.FC<Props> = ({ blogTitle, htmlContent, blogs }) => {
   return (
-    <section className="blog-section">
-      {/* Horizontal Navigation Bar */}
-      <nav className="flex justify-center">
-        <ul className="flex">
-          {categories.map((category) => (
-            <li
-              key={category}
-              className={`mr-4 ${selectedCategory === category ? 'underline' : ''
-                }`}
-            >
-              <a role="button" onClick={() => setSelectedCategory(category)}>
-                {category}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
-      {/* Banner */}
-      <div className="flex items-center justify-center relative mt-5">
-        <Image alt="blog banner" src={bannerImage} width={1246} height={413} />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-          <h1 className="">{selectedCategory}</h1>
-          <h1 className="text-3xl font-bold mt-10">
-            Richird Norton photorealistic rendering as real photos
-          </h1>
-          <p className="mt-10 font-thin">
-            The rendering of the project is a process that is carried out
-            throughout the project, from the first sketches to the final
-            presentation.
-          </p>
-        </div>
-      </div>
-
-      {/* Blog Cards Grid */}
-      <div className="flex justify-center mt-8">
+    <div className="container mx-auto mt-10 cols justify-center" style={{ width: "1080px" }}>
+      <h1 className="text-4xl font-bold">{blogTitle}</h1>
+      <div className="w-full text-justify mt-10" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <div className="container mx-auto mt-20 text-lg font-bold">CÓ THỂ BẠN QUAN TÂM</div>
+      <div className="flex justify-center mt-10">
         <div className="blog-grid grid grid-cols-3 gap-12">
-          {blogs.map((blog) => (
+          {blogs.slice(0, 3).map((blog) => (
             <BlogCard
               key={blog.id}
               id={blog.id}
@@ -78,21 +35,7 @@ const BlogSection: React.FC<BlogSectionProps> = ({
           ))}
         </div>
       </div>
-
-      <div className='mt-10 flex justify-center'>
-        <Pagination
-          paginationForm={paginationForm}
-          disable={false}
-          show={true}
-        />
-      </div>
-
-      <style jsx>{`
-        .underline {
-          text-decoration: underline;
-        }
-      `}</style>
-    </section>
+    </div>
   );
 };
 
