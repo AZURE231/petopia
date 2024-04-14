634e91f09868439134f92207e0bd120366c0e55d
ducanh <buingocducanh24@gmail.com>
2024-04-14 23:27:59 +0700

    update: USER PAGE - paginating owned pets

--- app/(pages)/user/[userId]/page.tsx
+++ app/(pages)/user/[userId]/page.tsx
@@ -1,7 +1,6 @@
 import { OtherUserInformation } from '@/src/components/user/OtherUserInformation';
 
 export default function page({ params }: { params: { userId: string } }) {
-  console.log(params.userId);
   return (
     <div className="mt-30">
       <OtherUserInformation userId={params.userId} />
