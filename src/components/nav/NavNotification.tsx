2e0583d701e4e50b06273778fe496b8d112ce694
Minh Hung <acubu6969@gmail.com>
2024-04-15 14:03:04 +0700

    notification edge case

--- src/components/nav/NavNotification.tsx
+++ src/components/nav/NavNotification.tsx
@@ -1,33 +1,54 @@
-import { MdNotifications, MdNotificationsActive } from "react-icons/md";
-import { useState } from "react";
+import { MdNotifications } from "react-icons/md";
+import { useEffect, useState } from "react";
 import { QUERY_KEYS } from "@/src/utils/constants";
 import { IApiResponse } from "@/src/interfaces/common";
 import { useQuery } from "@/src/utils/hooks";
 import { INotification } from "@/src/interfaces/notification";
-import { getNotifications } from "@/src/services/notification.api";
+import {
+  clearAll,
+  getNotifications,
+  markAsRead,
+} from "@/src/services/notification.api";
+import { set } from "mobx";
 
 export default function NavNotification() {
   const [showNotifications, setShowNotifications] = useState(false);
 
   const handleButtonClick = () => {
     setShowNotifications(!showNotifications);
+    markAsRead();
+    setIsNewNotification(false);
   };
 
   const [notifications, setNotifications] = useState<INotification[]>([]);
   const [error, setError] = useState(false);
+  const [isNewNotification, setIsNewNotification] = useState(false);
 
+  // GET NOTIFICATIONS
   useQuery<IApiResponse<INotification[]>>(
     [QUERY_KEYS.GET_NOTIFICATION],
     getNotifications,
     {
       onSuccess: (res) => {
         setNotifications(res.data.data);
-        console.log(res.data.data);
       },
       onError: () => setError(true),
       refetchOnWindowFocus: false,
     }
   );
+
+  useEffect(() => {
+    if (notifications.length > 0) {
+      const newNotification = notifications.find(
+        (notification) => !notification.isChecked
+      );
+      if (newNotification) {
+        setIsNewNotification(true);
+      } else {
+        setIsNewNotification(false);
+      }
+    }
+  }, [notifications]);
 
   const getTimeAgo = (createdAt: string): string => {
     const currentTime = new Date();
@@ -46,6 +67,11 @@ export default function NavNotification() {
     }
   };
 
+  const clearAll = () => {
+    clearAll();
+    setNotifications([]);
+  };
+
   return (
     <div className="relative font-[sans-serif] w-max mx-auto">
       <button
@@ -54,32 +80,45 @@ export default function NavNotification() {
       >
         <MdNotifications />
       </button>
+      {isNewNotification && (
+        <div className="absolute bg-red-500 text-white w-2 h-2 rounded-full flex items-center justify-center -top-0.5 -right-1">
+          <div className="w-1 h-1 bg-red-500 rounded-full"></div>
+        </div>
+      )}
       {showNotifications && (
         <div className="absolute shadow-lg bg-white py-2 z-[1000] min-w-full rounded-lg w-[380px] max-h-[500px] overflow-auto">
-          <div className="flex items-center justify-between my-4 px-4">
-            <p className="text-xs text-blue-500 cursor-pointer">Clear all</p>
-            <p className="text-xs text-blue-500 cursor-pointer">Mark as read</p>
+          <div className="flex items-center justify my-4 px-4">
+            <p
+              className="text-xs text-blue-500 cursor-pointer"
+              onClick={clearAll}
+            >
+              Clear all
+            </p>
           </div>
-          <ul className="divide-y">
-            {notifications.map((notification) => (
-              <li
-                key={notification.id}
-                className="py-4 px-4 flex items-center hover:bg-gray-50 text-black text-sm cursor-pointer"
-              >
-                <div className="ml-6">
-                  <h3 className="text-sm text-[#333] font-semibold">
-                    {notification.title}
-                  </h3>
-                  <p className="text-xs text-gray-400 mt-2">
-                    {notification.content}
-                  </p>
-                  <p className="text-xs text-blue-500 leading-3 mt-2">
-                    {getTimeAgo(notification.isCreatedAt)}
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          {notifications.length === 0 ? (
+            <p className="text-sm text-gray-400 px-4">Không có thông báo mới</p>
+          ) : (
+            <ul className="divide-y">
+              {notifications.map((notification) => (
+                <li
+                  key={notification.id}
+                  className="py-4 px-4 flex items-center hover:bg-gray-50 text-black text-sm cursor-pointer"
+                >
+                  <div className="ml-6">
+                    <h3 className="text-sm text-[#333] font-semibold">
+                      {notification.title}
+                    </h3>
+                    <p className="text-xs text-gray-400 mt-2">
+                      {notification.content}
+                    </p>
+                    <p className="text-xs text-blue-500 leading-3 mt-2">
+                      {getTimeAgo(notification.isCreatedAt)}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       )}
     </div>
