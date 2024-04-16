3ebb19579f1c41b548f1c2fa46604090a5dd577e
Minh Hung <acubu6969@gmail.com>
2024-04-16 15:24:44 +0700

    fix noti

--- src/services/notification.api.ts
+++ src/services/notification.api.ts
@@ -2,4 +2,4 @@ import { http } from './http';
   
 export const getNotifications = async () => await http.get('/Notification');
 export const markAsRead = async () => await http.get('/Notification/MarkAsSeen');
-export const clearAll = async () => await http.delete('/Notification');
+export const deleteNoti = async () => await http.delete('/Notification');
