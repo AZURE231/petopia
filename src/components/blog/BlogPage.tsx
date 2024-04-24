468afabf216a5eb20e8aa7a7d7a3bfe3d1c409db
Minh Hung <acubu6969@gmail.com>
2024-04-24 15:48:59 +0700

    blog page image style

--- src/components/blog/BlogPage.tsx
+++ src/components/blog/BlogPage.tsx
@@ -3,7 +3,6 @@ import BlogCard from "./BlogCard";
 import { IBlogResponse } from "@/src/interfaces/blog";
 
 
-
 interface Props {
   blogTitle: string;
   htmlContent: string;
@@ -11,14 +10,13 @@ interface Props {
 }
 
 const BlogPage: React.FC<Props> = ({ blogTitle, htmlContent, blogs }) => {
-  // Add CSS to auto-center images in the HTML content
-  const autoCenterImages = `<style>img { display: block; margin: 0 auto; }</style>`;
-  const autoCenteredHTMLContent = autoCenterImages + htmlContent;
+  // Modify only the images within the htmlContent
+  const styledHTMLContent = htmlContent.replace(/<img/g, '<img style="display: block; margin: 0 auto; max-width: 500px; height: 500px; object-fit:contain;"');
 
   return (
     <div className="container mx-auto mt-10 cols justify-center" style={{ width: "1080px" }}>
       <h1 className="text-4xl font-bold">{blogTitle}</h1>
-      <div className="w-full text-justify mt-10" dangerouslySetInnerHTML={{ __html: autoCenteredHTMLContent }} />
+      <div className="w-full text-justify mt-10" dangerouslySetInnerHTML={{ __html: styledHTMLContent }} />
       <div className="container mx-auto mt-20 text-lg font-bold">CÓ THỂ BẠN QUAN TÂM</div>
       <div className="flex justify-center mt-10">
         <div className="blog-grid grid grid-cols-3 gap-12">
