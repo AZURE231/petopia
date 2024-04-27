e809a8e52a65deca35527ed3d87a091a5c69bb95
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2024-04-27 21:29:03 +0700

    update post blog

--- src/services/blog.api.ts
+++ src/services/blog.api.ts
@@ -1,4 +1,4 @@
-import { IBlog } from "../interfaces/blog";
-import { http } from "./http";
+import { IBlog } from '../interfaces/blog';
+import { http } from './http';
 
-export const createBlog = async (data: IBlog) => await http.post('/Blog', data);
+export const postBlog = async (data: IBlog) => await http.post('/Blog', data);
