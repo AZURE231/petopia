30ee9ed410990d34af419b1d66d589cd2f9d3577
Minh Hung <acubu6969@gmail.com>
2024-05-03 10:42:53 +0700

    blog create card

---
+++ src/components/blog/BlogCreateCard.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { BLOG_CATEGORIES_OPTION } from '@/src/utils/constants';
+
+export default function BlogCreateCard() {
+  return (
+    <div className="relative">
+      <Link href={`/blog-create`}>
+        <div className="max-w-xs p-2 h-full bg-white border border-gray-200 rounded-2xl shadow-lg">
+          <div className="flex flex-col">
+            {/* Image with rounded corners */}
+            <div className="relative w-full pt-[100%]">
+              <div className="w-full h-full flex items-center justify-center bg-gray-200 border-dashed border border-gray-400 rounded-2xl">
+                <span className="text-4xl text-gray-400">+</span>
+              </div>
+            </div>
+            {/* Content */}
+            <div className="p-2 md:p-4">
+              {/* Title */}
+              <h2 className="text-xl font-bold mb-2 truncate">Tạo Blog</h2>
+              {/* Excerpt */}
+              {/* <p className="text-sm text-gray-600 line-clamp-3">{excerpt}</p> */}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
