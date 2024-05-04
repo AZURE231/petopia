3ef403362090aeaf19a6c30008fb5ec0aeaf0795
Minh Hung <acubu6969@gmail.com>
2024-05-04 12:14:04 +0700

    .

--- src/components/text-editor/BlogEditor.tsx
+++ src/components/text-editor/BlogEditor.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-import uploadAdapter from './UploadAdapter';
 import { IBlog, IBlogResponse } from '@/src/interfaces/blog';
 import { useForm } from 'react-hook-form';
 import { BLOG_CATEGORIES_OPTION, QUERY_KEYS } from '@/src/utils/constants';
@@ -11,16 +9,18 @@ import { getBlogDetail, postBlog } from '@/src/services/blog.api';
 import { useMutation, useQuery } from '@/src/utils/hooks';
 import { QueryProvider } from '../general/QueryProvider';
 import { Alert } from '../general/Alert';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import uploadAdapter from './UploadAdapter';
 
 const BlogEditor = QueryProvider(({ id = '' }: { id?: string }) => {
   // STATE
   const [editorData, setEditorData] = useState('');
-  const [editorLoaded, setEditorLoaded] = useState(false);
-
   const [alertShow, setAlertShow] = useState<boolean>(false);
   const [alertMessage, setAlertMessage] = useState<string>('');
   const [alertFailed, setAlertFailed] = useState<boolean>(false);
 
+  //dynamic import CKEditor so not to load it on the server
   const uploadImageForm = useForm<IUploadImage>({
     defaultValues: {
       showImages: [],
@@ -64,32 +64,6 @@ const BlogEditor = QueryProvider(({ id = '' }: { id?: string }) => {
     await uploadImage();
     postBlogMutation.mutate(createBlogForm.getValues());
   };
-
-  useEffect(() => {
-    if (!editorLoaded) {
-      setEditorLoaded(true);
-      import('@ckeditor/ckeditor5-build-classic')
-        .then((ClassicEditor) => {
-          ClassicEditor.default
-            .create(document.querySelector('#editor') as HTMLElement, {
-              extraPlugins: [uploadAdapter],
-            })
-            .then((editor: any) => {
-              editor.setData(editorData);
-              editor.model.document.on('change:data', () => {
-                setEditorData(editor.getData());
-              });
-            })
-            .catch((error: any) => {
-              console.error(
-                'There was a problem initializing the editor.',
-                error
-              );
-            });
-        })
-        .catch((error) => console.error('CKEditor load error:', error));
-    }
-  }, [editorData, editorLoaded]);
 
   // QUERY
   const postBlogMutation = useMutation<IApiResponse<string>, IBlog>(postBlog, {
@@ -167,7 +141,18 @@ const BlogEditor = QueryProvider(({ id = '' }: { id?: string }) => {
             </option>
           ))}
         </select>
-        <div id="editor" />
+        <CKEditor
+          editor={ClassicEditor as any}
+          data={editorData}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            createBlogForm.setValue('content', data);
+            setEditorData(data);
+          }}
+          config={{
+            extraPlugins: [uploadAdapter],
+          }}
+        />
         <button className="w-fit p-3 px-8 rounded-full font-bold shadow-md bg-yellow-300 hover:bg-yellow-400 my-5">
           Đăng bài
         </button>
