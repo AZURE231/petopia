970248088159fd695da6337c1ddf9e8f785e63ca
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2024-05-06 15:23:37 +0700

    fix pet filter

--- src/components/pet/BreedInput.tsx
+++ src/components/pet/BreedInput.tsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { useQuery } from '@/src/utils/hooks';
 import { QUERY_KEYS } from '@/src/utils/constants';
 import { IApiResponse } from '@/src/interfaces/common';
@@ -17,20 +17,26 @@ export default function BreedInput({
   const [breeds, setBreeds] = useState<string[]>(['Chưa rõ']);
 
   // QUERY
-  useQuery<IApiResponse<string[]>>(
+  const getBreedQuery = useQuery<IApiResponse<string[]>>(
     [QUERY_KEYS.GET_BREED_DETAIL, watch('species')],
     () => getBreed(watch('species')),
     {
       onSuccess: (res) => {
-        setBreeds((breed) => [...breed, ...res.data.data]);
+        console.log(watch('species'));
+        setBreeds(res.data.data);
       },
       enabled: watch('species') !== -1,
       refetchOnWindowFocus: false,
     }
   );
 
+  useEffect(() => {
+    setValue('breed', '');
+  }, [watch('species')]);
+
   return (
     <FilterDropDown
+      disabled={getBreedQuery.isLoading}
       options={breeds.map((e) => ({ label: e, value: e }))}
       value={watch('breed')}
       setValue={(value: string) => setValue('breed', value)}
