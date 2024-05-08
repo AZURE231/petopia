9559e9afdebaff3027db976b3ebce540dd3f5285
Minh Hung <acubu6969@gmail.com>
2024-05-08 11:10:29 +0700

    finish carousel with api refetch, titlle and des, link to blog

--- src/components/blog/BlogCarousel.tsx
+++ src/components/blog/BlogCarousel.tsx
@@ -1,39 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import { IBlogAd } from '@/src/interfaces/blog';
+import { IApiResponse } from '@/src/interfaces/common';
+import { getBlogAd } from '@/src/services/blog.api';
+import { QUERY_KEYS } from '@/src/utils/constants';
+import { useQuery } from '@/src/utils/hooks';
+import { useEffect, useState } from 'react';
 
 export default function BlogCarousel() {
-  const [activeIndex, setActiveIndex] = useState(0);
   const TRANSTION_DURATION = 5000;
-  const images = [
-    '/img/Banner.png',
-    '/img/blog_banner.png',
-    '/img/cat_hero.png',
-    '/img/blog_card.png',
-  ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [transitionDuration, setTransitionDuration] =
+    useState(TRANSTION_DURATION);
+  const [blogAd, setBlogAd] = useState<IBlogAd[]>([]);
+
+  const getAdQuery = useQuery<IApiResponse<IBlogAd[]>>(
+    [QUERY_KEYS.GET_BLOG_AD],
+    () => getBlogAd(),
+    {
+      onSuccess: (data) => {
+        setBlogAd(data.data.data);
+      },
+      onError: () => {
+        // Handle error
+      },
+    }
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setActiveIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, TRANSTION_DURATION);
-
+      setActiveIndex((prevIndex) => {
+        if (prevIndex === blogAd.length - 1) {
+          getAdQuery.refetch();
+          setTransitionDuration(TRANSTION_DURATION);
+          return 0;
+        } else {
+          return prevIndex + 1;
+        }
+      });
+    }, transitionDuration);
+    console.log('activeIndex', activeIndex);
     return () => clearInterval(interval);
-  }, []);
+  }, [activeIndex, blogAd.length, getAdQuery, transitionDuration]);
 
-  const goToSlide = (index: any) => {
+  const goToSlide = (index: number) => {
     setActiveIndex(index);
   };
 
   const goToPrevSlide = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      prevIndex === 0 ? blogAd.length - 1 : prevIndex - 1
     );
+    setTransitionDuration(TRANSTION_DURATION); // Reset transition duration
   };
 
   const goToNextSlide = () => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === blogAd.length - 1) {
+        getAdQuery.refetch();
+        setTransitionDuration(TRANSTION_DURATION); // Reset transition duration
+        return 0;
+      } else {
+        return prevIndex + 1;
+      }
+    });
   };
 
   return (
@@ -44,20 +72,35 @@ export default function BlogCarousel() {
       style={{ height: '413px', width: '100%' }}
     >
       <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        {images.map((image, index) => (
+        {blogAd?.map((item, index) => (
           <div
             key={index}
-            className={`absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition duration-700 ease-in-out transform ${
-              activeIndex === index ? 'opacity-100' : 'opacity-0'
+            className={`absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition duration-700 ease-in-out transform opacity-70 ${
+              activeIndex === index ? 'visible' : 'hidden'
             }`}
             data-carousel-item
           >
-            <img src={image} alt={`Slide ${index + 1}`} />
+            <img
+              src={item.image}
+              alt={`Slide ${index + 1}`}
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center">
+              <a href={`/blog/${blogAd[activeIndex].id}`}>
+                <h2 className="text-white mb-14 font-extrabold text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] transition duration-300 hover:underline hover:text-yellow-500">
+                  {item.title}
+                </h2>
+              </a>
+              <p className="text-gray-100 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                {item.excerpt}
+              </p>
+            </div>
           </div>
         ))}
       </div>
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        {images.map((_, index) => (
+
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-0.5 left-1/2 space-x-3 rtl:space-x-reverse">
+        {blogAd.map((_, index) => (
           <button
             key={index}
             type="button"
