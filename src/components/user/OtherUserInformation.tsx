23ef3e704b78da56fb1e2484254922fd35fdb13a
Đức Anh <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2024-05-12 15:11:53 +0700

    fix: BUGS  - #90, #86 (#104)

--- src/components/user/OtherUserInformation.tsx
+++ src/components/user/OtherUserInformation.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { QueryProvider } from '../general/QueryProvider';
 import ListCards from './ListCards';
-import { getOtherUserInfo, getPreReport } from '@/src/services/user.api';
+import { getOtherUserInfo } from '@/src/services/user.api';
 import { IUserInfoReponse } from '@/src/interfaces/user';
 import { IApiResponse, IPaginationModel } from '@/src/interfaces/common';
 import { useQuery } from '@/src/utils/hooks';
@@ -12,7 +12,7 @@ import {
   REPORT_ENTITY,
   USER_ROLE,
 } from '@/src/utils/constants';
-import { set, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { IPetResponse } from '@/src/interfaces/pet';
 import { getPetsByUser } from '@/src/services/pet.api';
 import Pagination from '../general/Pagination';
@@ -21,22 +21,15 @@ import { NameRoleBlock } from './NameRoleBlock';
 import Testimonials from '../general/Testimonials';
 import { BasicInfoBlock } from './BasicInfoBlock';
 import Image from 'next/image';
-import { GoReport } from 'react-icons/go';
-import Popup from 'reactjs-popup';
-import ReportUserForm from './ReportForm';
-import { Alert } from '../general/Alert';
+import { ReportBlock } from '../general/ReportBlock';
 
 export const OtherUserInformation = QueryProvider(
   ({ userId }: { userId: string }) => {
+
     // STATES
     const [userInfo, setUserInfo] = useState<IUserInfoReponse>();
     const [pets, setPets] = useState<IPetResponse[]>([]);
     const [userName, setUserName] = useState<string>('');
-    const [showReport, setShowReport] = useState(false);
-    const [isReported, setIsReported] = useState<boolean>(true);
-    const [alertShow, setAlertShow] = useState<boolean>(false);
-    const [alertMessage, setAlertMessage] = useState<string>('');
-    const [alertFailed, setAlertFailed] = useState<boolean>(false);
 
     // FORMS
     const paginationForm = useForm<IPaginationModel>({
@@ -77,17 +70,6 @@ export const OtherUserInformation = QueryProvider(
       }
     );
 
-    const getPreReportQuery = useQuery<IApiResponse<boolean>>(
-      [QUERY_KEYS.GET_PRE_REPORT],
-      () => getPreReport({ id: userId, entity: REPORT_ENTITY.User }),
-      {
-        onSuccess: (res) => {
-          setIsReported(res.data.data);
-        },
-        refetchOnWindowFocus: false,
-      }
-    );
-
     // EFFECTS
     useEffect(() => {
       if (userInfo?.role === USER_ROLE.ORGANIZATION) {
@@ -99,16 +81,6 @@ export const OtherUserInformation = QueryProvider(
           );
       }
     }, [userInfo]);
-
-    const handleShowReport = () => {
-      if (!isReported) {
-        setAlertMessage('Bạn đã báo cáo người dùng này');
-        setAlertFailed(true);
-        setAlertShow(true);
-        return;
-      }
-      setShowReport(true);
-    };
 
     return (
       <>
@@ -148,25 +120,10 @@ export const OtherUserInformation = QueryProvider(
             />
 
             <div className="mt-3 flex justify-end">
-              <Popup
-                modal
-                overlayStyle={{ background: 'rgba(0, 0, 0, 0.5)' }}
-                open={showReport}
-                onClose={() => setShowReport(false)}
-              >
-                <ReportUserForm
-                  preCheckQuery={getPreReportQuery}
-                  id={userId}
-                  type={REPORT_ENTITY.User}
-                  handleClose={() => setShowReport(false)}
-                />
-              </Popup>
-              <button
-                className="border border-black p-3 rounded-lg font-bold shadow-md bg-yellow-300 hover:bg-yellow-400 ml-2"
-                onClick={handleShowReport}
-              >
-                <GoReport size={25} />
-              </button>
+              <ReportBlock
+                id={userId}
+                type={REPORT_ENTITY.User}
+              />
             </div>
           </div>
         )}
@@ -180,12 +137,6 @@ export const OtherUserInformation = QueryProvider(
             }
           />
         </div>
-        <Alert
-          message={alertMessage}
-          show={alertShow}
-          setShow={setAlertShow}
-          failed={alertFailed}
-        />
       </>
     );
   }
