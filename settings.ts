2094b82f1731d2ab2a273e8912e7c8a46bc06f4b
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2024-06-03 08:48:27 +0700

    add link

--- settings.ts
+++ settings.ts
@@ -1,3 +1,3 @@
 export const API_ROUTE =
-  'https://2c40-2402-800-63b8-8671-9b6-6ce3-d326-deab.ngrok-free.app/api';
-export const DETECTION_ROUTE = 'https://1ec1-14-241-225-5.ngrok-free.app';
+  'https://deb9-2402-800-63b8-8671-2011-19f7-b6a6-cff7.ngrok-free.app/api';
+export const DETECTION_ROUTE = 'https://4873-14-241-225-2.ngrok-free.app';
